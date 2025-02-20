6eead28a645bd40995dea7c95fd53085b2c41121
Fouad <fouadezari@gmail.com>
2025-02-20 12:09:15 +0100

    refactor: améliorer les styles globaux et la structure des composants RichText pour une meilleure réactivité et accessibilité

--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -8,8 +8,8 @@ import RichText from '@/components/RichText'
 
 export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-      <div className="container mb-8">
+    <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-8 gap-4 items-center container h-auto mt-40 lg:mt-32">
+      <div className="mb-8 self-center lg:col-span-2 lg:col-end-3">
         {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
 
         {Array.isArray(links) && links.length > 0 && (
@@ -24,7 +24,7 @@ export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richTex
           </ul>
         )}
       </div>
-      <div className="overflow-hidden ">
+      <div className="lg:col-span-2 lg:col-end-5 place-self-end">
         {media && typeof media === 'object' && (
           <div>
             <Media
